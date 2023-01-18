c786b161ec03f58bb27b7daaac7319ecd068af21
Tejas-Naik <RNTejas2005@gmail.com>
2023-01-18 14:10:17 +0000

    React renders whenever the new props are passed and setState is called

--- monsters-robolox/src/components/card-list/card-list.component.jsx
+++ monsters-robolox/src/components/card-list/card-list.component.jsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 
 class CardList extends Component {
     render() {
-        console.log(this.props.monsters)        // when different props are passed the dom renders (and setState changes)
+        console.log(this.props.monsters)            // when different props are passed the dom renders (and setState changes)
         console.log("render from CardList");
         const { monsters } = this.props;
         return (
