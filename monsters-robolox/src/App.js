afe86685713e79eb3a09bd46b153ce88ec830b05
Tejas-Naik <RNTejas2005@gmail.com>
2023-01-18 10:05:33 +0000

    Class Components in React and setState()

--- monsters-robolox/src/App.js
+++ monsters-robolox/src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
+import { Component } from 'react';
 import './App.css';
+import logo from './logo.svg';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      name: { firstName: "Tejas", lastName: "Naik" },
+      company: 'Facebook'
+    }
+  }
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Hi, I am  {this.state.name.firstName} {this.state.name.lastName}. I work at  {this.state.company}
+          </p>
+          <button onClick={() => {
+            this.setState({ name: { firstName: "Andrei", lastName: "Neaogie" } });
+          }}>
+            Change Name
+          </button>
+
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
