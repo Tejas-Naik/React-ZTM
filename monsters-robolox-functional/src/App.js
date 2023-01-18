484999e21bce94ff822f75050ae693436d0ed20b
Tejas-Naik <RNTejas2005@gmail.com>
2023-01-18 16:32:14 +0000

    Functional Components basics

--- monsters-robolox-functional/src/App.js
+++ monsters-robolox-functional/src/App.js
@@ -1,59 +1,83 @@
-import { Component } from 'react';
+import { setSelectionRange } from '@testing-library/user-event/dist/utils';
+import { useState } from 'react';
 import './App.css';
 import CardList from './components/card-list/card-list.component';
 import SearchBox from './components/search-box/search-box.component';
 
-class App extends Component {
-  constructor() {
-    super();
+const App = () => {
+  console.log("render");
+  const [searchField, setSearchField] = useState('')
+  console.log(searchField);
 
-    this.state = {
-      monsters: [],
-      searchField: '',
-    }
+  const onSearchChange = (e) => {
+    const searchFieldString = e.target.value.toLowerCase();
+    setSearchField(searchFieldString);
   }
 
-  // When it first loads (mounts to the dom) only happens once
-  async componentDidMount() { // its a lifycycle method
-    const response = await fetch("https://jsonplaceholder.typicode.com/users");
-    const users = await response.json();
-    this.setState(
-      () => {
-        return { monsters: users }
-      }
-    )
-  }
-
-  onSearchChange = (e) => {
-    const searchField = e.target.value.toLowerCase();
-    this.setState(() => {
-      return {
-        searchField
-      }
-    });
-  }
-
-  render() {
-
-    const { monsters, searchField } = this.state;
-    const { onSearchChange } = this;
-    const filteredMonsters = monsters
-      .filter((monster) => monster.name
-        .toLowerCase()
-        .includes(searchField));
-
-    return (
-      <div className="App">
-        <h1 className='app-title'>Monsters Rolodex</h1>
-        <SearchBox
-          className="monsters-search-box"
-          placeholder="Search Monsters"
-          onChangeHandler={onSearchChange}
-        />
-        <CardList monsters={filteredMonsters} />
-      </div>
-    );
-  }
+  return (
+    <div className="App">
+      <h1 className='app-title'>Monsters Rolodex</h1>
+      <SearchBox
+        className="monsters-search-box"
+        placeholder="Search Monsters"
+        onChangeHandler={onSearchChange}
+      />
+      {/* <CardList monsters={filteredMonsters} /> */}
+    </div>
+  )
 }
+
+// class App extends Component {
+//   constructor() {
+//     super();
+
+//     this.state = {
+//       monsters: [],
+//       searchField: '',
+//     }
+//   }
+
+//   // When it first loads (mounts to the dom) only happens once
+//   async componentDidMount() { // its a lifycycle method
+//     const response = await fetch("https://jsonplaceholder.typicode.com/users");
+//     const users = await response.json();
+//     this.setState(
+//       () => {
+//         return { monsters: users }
+//       }
+//     )
+//   }
+
+//   onSearchChange = (e) => {
+//     const searchField = e.target.value.toLowerCase();
+//     this.setState(() => {
+//       return {
+//         searchField
+//       }
+//     });
+//   }
+
+//   render() {
+
+//     const { monsters, searchField } = this.state;
+//     const { onSearchChange } = this;
+//     const filteredMonsters = monsters
+//       .filter((monster) => monster.name
+//         .toLowerCase()
+//         .includes(searchField));
+
+//     return (
+//       <div className="App">
+//         <h1 className='app-title'>Monsters Rolodex</h1>
+//         <SearchBox
+//           className="monsters-search-box"
+//           placeholder="Search Monsters"
+//           onChangeHandler={onSearchChange}
+//         />
+//         <CardList monsters={filteredMonsters} />
+//       </div>
+//     );
+//   }
+// }
 
 export default App;
