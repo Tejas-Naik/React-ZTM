61737f031e4218348071335c56f4947ad8fbbe73
Tejas-Naik <RNTejas2005@gmail.com>
2023-01-18 18:01:22 +0000

    Converting everything to Functional components and using React 18

--- monsters-robolox-functional/src/components/card-contaner/card-container.component.jsx
+++ monsters-robolox-functional/src/components/card-contaner/card-container.component.jsx
@@ -1,15 +1,14 @@
-import React, { Component } from 'react'
-import './card-container.styles.css'
+import React from 'react';
+import './card-container.styles.css';
 
-export default class CardContainer extends Component {
-    render() {
-        const { name, id, email } = this.props;
-        return (
-            <div className='card-container' key={id}>
-                <img alt={`monster ${name}`} src={`https://robohash.org/${id}?set=set2&size=180x180`} />
-                <h2>{name}</h2>
-                <p>{email}</p>
-            </div>
-        )
-    }
+function CardContainer({ name, id, email }) {
+    return (
+        <div className='card-container' key={id}>
+            <img alt={`monster ${name}`} src={`https://robohash.org/${id}?set=set2&size=180x180`} />
+            <h2>{name}</h2>
+            <p>{email}</p>
+        </div>
+    )
 }
+
+export default CardContainer;
